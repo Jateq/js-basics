b50894c704d74c80578a891fffa8cb741c85d055
Jateq <temirlan.eraly1@gmail.com>
2023-05-16 22:06:21 +0600

    leetcode

---
+++ basics/leetcode-js-30/day1.js
@@ -0,0 +1,19 @@
+//** 
+// 2667. Create Hello World Function
+// Easy
+// Write a function createHelloWorld. It should return
+// a new function that always returns "Hello World"
+
+/**
+ * @return {Function}
+ */
+
+var createHelloWorld = function() {
+    return () => "Hello World";
+};
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
---
+++ basics/leetcode-js-30/day3.js
@@ -0,0 +1,55 @@
+// 2665. Counter II
+// Easy
+
+// Write a function createCounter. It should accept an initial integer init. 
+// It should return an object with three functions.
+
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let counter = init;
+    let resetVal = init;
+
+    let obj = {
+        increment : () => ++counter,
+        decrement : () => --counter,
+        reset : () => counter = resetVal
+    }
+
+    return obj;
+};
+
+
+// var createCounter = function(init) {
+//     c = i = init
+
+//     function increment() {
+//         return ++c
+//     }
+
+//     function decrement() {
+//         return --c
+//     }
+
+//     function reset() {
+//         return c = i
+//     }
+//     return {increment, decrement, reset}
+// };
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
