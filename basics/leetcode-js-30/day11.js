2673d82406f562e763129953dc911b6de96bd3b0
Jateq <temirlan.eraly1@gmail.com>
2023-05-21 16:07:19 +0600

    day 11

---
+++ basics/leetcode-js-30/day11.js
@@ -0,0 +1,21 @@
+// 2621. Sleep
+// Easy
+
+/**
+ * Given a positive integer millis, write an asyncronous function that  
+ * sleeps for millis milliseconds. It can resolve any value.
+ * 
+ * @param {number} millis
+ * 
+ */
+
+async function sleep(millis) {
+    return new Promise(time => {
+        setTimeout(time, millis)
+    })
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
