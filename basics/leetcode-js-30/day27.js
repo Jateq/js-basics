d12511b69bd8d2e85bece64890acfa2fa7f57c4e
Jateq <temirlan.eraly1@gmail.com>
2023-06-01 22:48:28 +0600

    day 27

---
+++ basics/leetcode-js-30/day27.js
@@ -0,0 +1,40 @@
+// 2694. Event Emitter
+// Medium
+
+// Design an EventEmitter class. This interface is similar (but with some differences) to the one found in Node.js or the Event Target interface of the DOM. The EventEmitter should allow for subscribing to events and emitting them.
+
+
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    subscribe(event, cb) {
+      this.events[event] = this.events[event] ?? [];
+      this.events[event].push(cb);
+
+      return {
+        unsubscribe: () => {
+          this.events[event] = this.events[event].filter(f => f !== cb);
+          //To avoid memory leaks adding a cleanup condition
+          if (this.events[event].length === 0) { delete this.events[event] }
+        },
+      };
+    }
+
+    emit(event, args = []) {
+        if (!(event in this.events)) return [];
+        return this.events[event].map(f => f(...args));
+    }
+}
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
